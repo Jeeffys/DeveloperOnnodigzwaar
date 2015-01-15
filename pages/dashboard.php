050fad324d1f73a5d86790d8d5615bb88efd5918
larsvankleef <larsvankleef@outlook.com>
2015-01-15 16:23:17 +0100

    pdf template maken toegevoegd

--- pages/dashboard.php
+++ pages/dashboard.php
@@ -3,18 +3,8 @@
 <nav class="navbar navbar-default" role="navigation">
   <div class="container-fluid">
     <div class="navbar-header">
-      <a class="navbar-brand" href="index.php?page=dashboard/boordeel">
-        Beoordeel student
-      </a>
-    </div>
-  </div>
-</nav>
-
-<nav class="navbar navbar-default" role="navigation">
-  <div class="container-fluid">
-    <div class="navbar-header">
-      <a class="navbar-brand" href="index.php?page=dashboard/voortgang">
-        Voortgang student
+      <a class="navbar-brand" href="index.php?page=dashboard/template">
+        Pdf template maken
       </a>
     </div>
   </div>
